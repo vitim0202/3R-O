1d18b3ef7a9924327723aad793c69d2930e4affb
vitor <105921994+vitim0202@users.noreply.github.com>
2024-03-12 10:24:23 -0300

    141

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const botoes= document.querySelectorA11("botao");
 for( let i = 0; i <botoes.length; i++){
     botoes[i].onclick=function(){
             for (let j = 0; j <botoes.length; j++){
+                botoes[j].classlist.remove("ativo")
             }
         botoes[i].classlist.add("ativo")
     }
