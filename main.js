fba20d77d0e58834f5eb12cdec42077f1511fc9e
vitor <105921994+vitim0202@users.noreply.github.com>
2024-03-12 10:33:41 -0300

    sss

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const botoes= document.querySelectorA11("botao");
 for( let i = 0; i <botoes.length; i++){
     botoes[i].onclick=function(){
             for (let j = 0; j <botoes.length; j++){
-                botoes[j].classlist.remove("ativo")
+                botoes[j].cLasslist.remove("ativo");
             }
-        botoes[i].classlist.add("ativo")
+        botoes[i].cLasslist.add("ativo")
     }
 }
