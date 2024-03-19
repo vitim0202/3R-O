9a558ea8323dc6f993fae724b516f45cb2da2553
vitor <105921994+vitim0202@users.noreply.github.com>
2024-03-19 09:24:19 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const botoes= document.querySelectorAll("botao");
+const botoes= document.querySelectorAll(".botao");
 for( let i = 0; i <botoes.length; i++){
     botoes[i].onclick=function(){
             for (let j = 0; j <botoes.length; j++){
