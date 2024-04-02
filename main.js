84f2a35a181ba8ea1b5aa396bbd111cdc1c4a1d3
vitor <105921994+vitim0202@users.noreply.github.com>
2024-04-02 10:33:05 -0300

    ggg

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ for( let i = 0; i <botoes.length; i++){
     botoes[i].onclick=function(){
             for (let j = 0; j <botoes.length; j++){
                 botoes[j].classList.remove("ativo");
+                textos[j].classList.remove("ativo");
             }
-        botoes[i].classList.add("ativo");
-        textos[j].classList.remove("ativo");
+       
     }
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
