f637dd7bcb7b986b94c48357f67483c19733c983
vitor <105921994+vitim0202@users.noreply.github.com>
2024-04-02 10:32:06 -0300

    hhh

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes= document.querySelectorAll(".botao");
 const textos= document.querySelectorAll(".aba-conteudo");
+
 for( let i = 0; i <botoes.length; i++){
     botoes[i].onclick=function(){
             for (let j = 0; j <botoes.length; j++){
@@ -10,5 +11,5 @@ for( let i = 0; i <botoes.length; i++){
     }
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
-
+}
 
